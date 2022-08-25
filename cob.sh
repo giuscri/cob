748c52dd7c392512749e842d55a892606c591913
Giuseppe Crinò <giuscri@gmail.com>
2022-08-25 18:12:13 +0200

    feat: provide better ergonomics

--- cob.sh
+++ cob.sh
@@ -1,18 +1,20 @@
-# Returns HEAD commit message formatted as a valid branch name
-msg2bname() {
-  _msg2bname "$(git show -s --format=%s)"
+cob() {
+  msg="$(git show -s --format=%s)"
+  git checkout -b "$(_msg_to_bname "$msg")"
 }
 
-_msg2bname() {
+cm() {
+  bname="$(git branch --show-current)"
+  git commit -m "$(_bname_to_msg "$bname")" --edit
+}
+
+# Returns HEAD commit message formatted as a valid branch name
+_msg_to_bname() {
   echo $1 | tr '(' '_' | tr -d '):' | tr ' ' '-'
 }
 
 # Returns branch name formatted as a commit message
-bname2msg() {
-  _bname2msg "$(git branch --show-current)"
-}
-
-_bname2msg() {
+_bname_to_msg() {
   bname=$1
   if [[ $(echo $bname | grep '_') ]]; then
     echo $bname | sed 's,_\([^-]*\),(\1):,' | tr '-' ' '
--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ assert() {
     [[ "$expected" = "$actual" ]] && echo "PASS" || echo "FAIL: $expected != $actual"
 }
 
-assert "chore_k8s-create-secret" "$(_msg2bname 'chore(k8s): create secret')"
-assert "chore-use-correct-db-password" "$(_msg2bname 'chore: use correct db password')"
+assert "chore_k8s-create-secret" "$(_msg_to_bname 'chore(k8s): create secret')"
+assert "chore-use-correct-db-password" "$(_msg_to_bname 'chore: use correct db password')"
 
-assert "chore(k8s): create secret" "$(_bname2msg 'chore_k8s-create-secret')"
-assert "chore: use correct db password" "$(_bname2msg 'chore-use-correct-db-password')"
+assert "chore(k8s): create secret" "$(_bname_to_msg 'chore_k8s-create-secret')"
+assert "chore: use correct db password" "$(_bname_to_msg 'chore-use-correct-db-password')"
